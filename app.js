013d5532f48477cfaa1615e16cad121b29d693dc
Robert Rydlewski <robertrydlewski1984@gmail.com>
2019-06-02 18:16:29 +0200

    commit switch statemnt and conditional statement in the fucnction

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+/* Chart used in this code is from website : https://www.the-house.com/helpdesk/snowboard-sizing/ */
 var value = document.getElementById("myList").value; 
-
+/* using switch statement inside the function */
 function snowboardSize()  {
     var value = document.getElementById("myList").value; 
     switch(value) {
         case "109cm":
-            document.write("Your snowboard size is between 110cm and 120cm" );
+            document.write("Your snowboard size is between 90cm and 105cm");
           break;
         case "124cm":
             document.write("Your snowboard size is between 110cm and 120cm" );
@@ -12,9 +13,55 @@ function snowboardSize()  {
         case "137cm":
             document.write("Your snowboard size is between 115cm and 130cm" );
           break;
+        case "147cm":
+            document.write("Your snowboard size is between 125cm and 135cm" );
+          break;
+          case "155cm":
+            document.write("Your snowboard size is between 130cm and 140cm" );
+          break;
+        case "160cm":
+            document.write("Your snowboard size is between 135cm and 145cm" );
+          break;
+        case "163cm":
+            document.write("Your snowboard size is between 140cm and 150cm" );
+          break;
+        case "165cm":
+            document.write("Your snowboard size is between 145cm and 152cm" );
+          break;
+          case "168cm":
+            document.write("Your snowboard size is between 148cm and 153cm" );
+          break;
+        case "170cm":
+            document.write("Your snowboard size is between 150cm and 155cm" );
+          break;
+        case "173cm":
+            document.write("Your snowboard size is between 152cm and 155cm" );
+          break;
+        case "178cm":
+            document.write("Your snowboard size is between 154cm and 159cm" );
+          break;
+          case "180cm":
+            document.write("Your snowboard size is between 155cm and 160cm" );
+          break;
+        case "183cm":
+            document.write("Your snowboard size is between 156cm and 162cm"  );
+          break;
+        case "185cm":
+            document.write("Your snowboard size is between 157cm and 163cm");
+          break;
+        case "188cm":
+            document.write("Your snowboard size is between 158cm and 166cm");
+          break;
+        case "191cm":
+            document.write("Your snowboard size is between 159cm and 167cm" );
+          break;
+        case "193cm":
+            document.write("Your snowboard size is between 160cm and 170cm" );
+          break;
         default:
           text = "Snowboarding is the best lol...";
       }
-      window.onclick = document.getElementById('myList').onclick = snowboardSize;
+      /* calling the switch statement in the function */
+      window.onclick = document.getElementById('myList').onclick = snowboardSize();
 
 }
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* Chart used in this code is from website : https://www.the-house.com/helpdesk/snowboard-sizing/ */
-
+/* using function with conditional staement */
 function snowboardSize()  {
     var value = document.getElementById("myList").value;
     if (value === "109cm") {
@@ -7,42 +7,64 @@ function snowboardSize()  {
     }
         else if (value === "124cm" ) {
         document.write("Your snowboard size is between 110cm and 120cm" );
+
     }   else if (value === "124cm" ) {
         document.write("Your snowboard size is between 110cm and 120cm" );
+
     }   else if (value === "137cm" ) {
         document.write("Your snowboard size is between 115cm and 130cm" );
+
     } else if (value === "147cm" ) {
         document.write("Your snowboard size is between 125cm and 135cm" );
+
     } else if (value === "155cm" ) {
         document.write("Your snowboard size is between 130cm and 140cm" );
+
     } else if (value === "160cm" ) {
         document.write("Your snowboard size is between 135cm and 145cm" );
+
     } else if (value === "163cm" ) {
         document.write("Your snowboard size is between 140cm and 150cm" );
+
     } else if (value === "165cm" ) {
         document.write("Your snowboard size is between 145cm and 152cm" );
+
     } else if (value === "168cm" ) {
         document.write("Your snowboard size is between 148cm and 153cm" );
+
     } else if (value === "170cm" ) {
         document.write("Your snowboard size is between 150cm and 155cm" );
+
     } else if (value === "173cm" ) {
         document.write("Your snowboard size is between 152cm and 155cm" );
+
     } else if (value === "175cm" ) {
         document.write("Your snowboard size is between 154cm and 159cm" );
+
+    } else if (value === "178cm" ) {
+        document.write("Your snowboard size is between 154cm and 159cm" );
+
     } else if (value === "180cm" ) {
         document.write("Your snowboard size is between 155cm and 160cm" );
+
     } else if (value === "183cm" ) {
         document.write("Your snowboard size is between 156cm and 162cm" );
+
     } else if (value === "185cm" ) {
         document.write("Your snowboard size is between 157cm and 163cm" );
+
     } else if (value === "188cm" ) {
         document.write("Your snowboard size is between 158cm and 166cm" );
+
     } else if (value === "191cm" ) {
         document.write("Your snowboard size is between 159cm and 167cm" );
+
     } else if (value === "193cm" ) {
         document.write("Your snowboard size is between 160cm and 170cm" );
+
      } else {
     document.write("Lake Tahoe is one of the best spots in the world lol.")
     }
-    window.onclick = document.getElementById('myList').onclick = snowboardSize;
+    /* calling function with conditional statemnet*/
+    window.onclick = document.getElementById('myList').onclick = snowboardSize();
 }
